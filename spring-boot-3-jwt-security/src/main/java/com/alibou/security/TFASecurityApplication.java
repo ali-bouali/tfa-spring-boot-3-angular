45b3fa27ca727a73af89210b6ad98cb2f79f6efe
ali-bouali <bouali.pro@gmail.com>
2023-08-05 15:31:01 +0200

    rename main class & enhance diagram

--- spring-boot-3-jwt-security/src/main/java/com/alibou/security/TFASecurityApplication.java
+++ spring-boot-3-jwt-security/src/main/java/com/alibou/security/TFASecurityApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class TFASecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+		SpringApplication.run(TFASecurityApplication.class, args);
 	}
 }
